ada1a3448f1d15d042de4b27ee3944ad62d8c652
KimJaeguk <kempire902@gmail.com>
2024-06-11 17:49:22 +0900

    nstep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,7 @@ android {
 
 dependencies {
 
-    implementation ("androidx.compose.ui:ui:1.4.0")
-    implementation ("androidx.compose.material3:material3:1.1.0-alpha01")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.0")
-    implementation ("androidx.activity:activity-compose:1.6.0")
-    implementation ("androidx.navigation:navigation-compose:2.5.0")
-    implementation ("com.google.android.gms:play-services-auth:20.0.0")
-    implementation ("io.github.jan-tennert.supabase:supabase-kt:0.7.0")
+    implementation ("io.supabase:supabase-kt:0.0.3")
     implementation ("io.coil-kt:coil-compose:2.1.0")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.activity:activity-compose")
