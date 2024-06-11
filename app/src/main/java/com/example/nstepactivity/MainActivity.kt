2634e07a65ead93e7aed4159fd6c65d93811628b
KimJaeguk <kempire902@gmail.com>
2024-06-11 18:14:07 +0900

    nstep

--- app/src/main/java/com/example/nstepactivity/MainActivity.kt
+++ app/src/main/java/com/example/nstepactivity/MainActivity.kt
@@ -10,6 +10,7 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -24,9 +25,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.nstepactivity.ui.theme.NstepActivityTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -35,6 +45,7 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
 
     private var nscoinCount by mutableStateOf(0)
+    private var stepCount by mutableStateOf(0L)
     private var selectedCats by mutableStateOf(emptyList<Int>())
     private var catCollection = mutableStateListOf<Int>()
 
@@ -60,9 +71,12 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MainScreen() {
-        var stepCount by remember { mutableStateOf(0L) }
-
         val context = LocalContext.current
+        val activity = (context as? Activity)
+        val displayMetrics = DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
+        val screenSize = IntSize(displayMetrics.widthPixels, displayMetrics.heightPixels)
+
         val sensorManager = context.getSystemService(SensorManager::class.java)
         val sensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
 
@@ -85,65 +99,80 @@ class MainActivity : ComponentActivity() {
             sensorManager?.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_UI)
         }
 
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "발걸음 수: $stepCount")
-            Text(text = "NSCoins: $nscoinCount", modifier = Modifier.padding(bottom = 16.dp))
-            Button(onClick = {
-                val intent = Intent(context, CatDraw::class.java)
-                intent.putExtra("nscoinCount", nscoinCount)
-                intent.putIntegerArrayListExtra("catCollection", ArrayList(catCollection))
-                resultLauncher.launch(intent)
-            }) {
-                Text(text = "고양이 뽑으러 가기")
-            }
-            Button(onClick = {
-                val intent = Intent(context, CatBook::class.java)
-                intent.putIntegerArrayListExtra("catCollection", ArrayList(catCollection))
-                context.startActivity(intent)
-            }) {
-                Text(text = "고양이 도감 보러 가기")
-            }
-
-            Box(modifier = Modifier.fillMaxSize()) {
-                Image(
-                    painter = painterResource(id = R.drawable.grass_background),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize()
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.grass_background),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize().padding(16.dp)
+            ) {
+                Text(
+                    text = "발걸음 수: $stepCount",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
-                CatAnimation(selectedCats)
+                Text(
+                    text = "NSCoins: $nscoinCount",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Button(onClick = {
+                    val intent = Intent(context, CatDraw::class.java)
+                    intent.putExtra("nscoinCount", nscoinCount)
+                    intent.putIntegerArrayListExtra("catCollection", ArrayList(catCollection))
+                    resultLauncher.launch(intent)
+                }) {
+                    Text(text = "고양이 뽑으러 가기")
+                }
+                Button(onClick = {
+                    val intent = Intent(context, CatBook::class.java)
+                    intent.putIntegerArrayListExtra("catCollection", ArrayList(catCollection))
+                    context.startActivity(intent)
+                }) {
+                    Text(text = "고양이 도감 보러 가기")
+                }
+
+                if (catCollection.isNotEmpty()) {
+                    CatAnimation(catCollection, screenSize)
+                }
             }
         }
     }
 
     @Composable
-    fun CatAnimation(selectedCats: List<Int>) {
-        val cats = listOf(
-            R.drawable.runa,
-            R.drawable.mailo,
-            R.drawable.oliver,
-            R.drawable.reo,
-            R.drawable.nero,
-            R.drawable.cheeze
-        )
-
+    fun CatAnimation(catCollection: List<Int>, screenSize: IntSize) {
         val coroutineScope = rememberCoroutineScope()
 
-        selectedCats.forEach { catIndex ->
+        catCollection.forEach { catIndex ->
             val xPos = remember { Animatable(0f) }
             val yPos = remember { Animatable(0f) }
 
+            val density = LocalDensity.current
+
             LaunchedEffect(Unit) {
                 coroutineScope.launch {
+                    val catSizePx = with(density) { 100.dp.toPx() }
+
                     while (true) {
+                        val newX = Random.nextFloat() * (screenSize.width - catSizePx)
+                        val newY = Random.nextFloat() * (screenSize.height - catSizePx)
+
                         xPos.animateTo(
-                            targetValue = Random.nextFloat() * 1000,
+                            targetValue = newX,
                             animationSpec = tween(durationMillis = 5000, easing = LinearEasing)
                         )
                         yPos.animateTo(
-                            targetValue = Random.nextFloat() * 1000,
+                            targetValue = newY,
                             animationSpec = tween(durationMillis = 5000, easing = LinearEasing)
                         )
                         delay(500)
@@ -151,12 +180,34 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            Image(
-                painter = painterResource(id = cats[catIndex]),
+            AsyncImage(
+                model = "file:///android_asset/${getCatImageResource(catIndex)}",
                 contentDescription = null,
-                modifier = Modifier.size(100.dp)
-                    .offset(xPos.value.dp, yPos.value.dp)
+                modifier = Modifier
+                    .size(100.dp)
+                    .offset { IntOffset(xPos.value.toInt(), yPos.value.toInt()) }
+                    .layout { measurable, constraints ->
+                        val placeable = measurable.measure(constraints)
+                        layout(constraints.maxWidth, constraints.maxHeight) {
+                            placeable.placeRelative(
+                                x = xPos.value.toInt().coerceIn(0, screenSize.width - placeable.width),
+                                y = yPos.value.toInt().coerceIn(0, screenSize.height - placeable.height)
+                            )
+                        }
+                    }
             )
+        }
+    }
+
+    private fun getCatImageResource(catIndex: Int): String {
+        return when (catIndex) {
+            0 -> "runa_m.gif"
+            1 -> "mailo_m.gif"
+            2 -> "oliver_m.gif"
+            3 -> "reo_m.gif"
+            4 -> "nero_m.gif"
+            5 -> "cheeze_m.gif"
+            else -> ""
         }
     }
 
@@ -168,10 +219,6 @@ class MainActivity : ComponentActivity() {
                 val newSelectedCats =
                     data?.getIntegerArrayListExtra("selectedCats")?.toList() ?: emptyList()
 
-                // 기존 선택된 고양이 리스트 업데이트
-                selectedCats = newSelectedCats
-
-                // 새로운 고양이를 선택하거나 이미 보유한 고양이를 다시 선택한 경우 NS 코인 추가
                 newSelectedCats.forEach { catIndex ->
                     if (!catCollection.contains(catIndex)) {
                         catCollection.add(catIndex)
@@ -179,6 +226,8 @@ class MainActivity : ComponentActivity() {
                         nscoinCount += 10
                     }
                 }
+
+                selectedCats = newSelectedCats
             }
         }
 }
